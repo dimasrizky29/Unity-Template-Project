4b3968855e62ea956aeb8bae1ab59548f6a4b66c
DESKTOP-DGCLESA\pc1 <dimasrizkyaf@gmail.com>
2026-04-13 10:29:41 +0700

    Update package

--- Assets/Scripts/Core/Other/GlobalResolver.cs
+++
@@ -1,7 +0,0 @@
-using VContainer;
-
-public static class GlobalResolver
-{
-    // Ini akan menampung Resolver dari ProjectLifetimeScope
-    public static IObjectResolver Instance { get; set; }
-}
--- Assets/Scripts/Domain/Audio/AudioType.cs
+++
@@ -1,6 +0,0 @@
-public enum AudioType
-{
-    BGM,
-    SFX,
-    UI
-}
